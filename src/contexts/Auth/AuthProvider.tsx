39c24156fd75159f667e979a0e0e8f798bc44fde
kevinferreiradev <ferreirakevin325@gmail.com>
2022-02-17 15:49:20 -0300

    create logic signin e logout +token

--- src/contexts/Auth/AuthProvider.tsx
+++ src/contexts/Auth/AuthProvider.tsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import { useApi } from "../../hooks/useApi";
+import { User } from "../../types/User";
 import { AuthContext } from "./AuthContext";
 
 export const AuthProvider = ({ children }: {children: JSX.Element }) => {
 
-    
+    const [user, setUser] = useState<User | null>(null);
+    const api = useApi();
+
+    const signin = async (email: string, password: string) => {
+        const data = await api.signin(email, password);
+        if(data.user && data.token) {
+            setUser(data.user);
+            return true;
+        }
+        return false;
+    }
+
+    const singout = async () => {
+        await api.logout();
+        setUser(null);
+    }
 
     return (
         <AuthContext.Provider value={{ user, signin, singout }}>
